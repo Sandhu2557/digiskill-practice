9825fae5f1fad2a40e1fe6328e53b653bac05730
Sandhu2557 <ua8546561@gmail.com>
2026-09-09 16:45:27 +0500

    Add initial implementation of primitive data types in JavaScript

---
+++ datatypes.js
@@ -0,0 +1,15 @@
+// Primitive data types
+// string, number (int, float), boolean, undefined, null
+
+let name = "Usman";  //string 
+let age = "26";     //string for JS anything in "is string"
+
+let height = 5.5; // number
+let weight = 35;   // number
+
+let isstudent = true; //boolean
+
+let test; //undefined is datatype in Javascript 
+
+let city = null; //null also datatype in Javascript
+console.log(city)
